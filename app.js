0f635003cc319317e38184bfa96964bac53858bd
Guilherme Eduardo Bublitz <gui.e.bublitz@gmail.com>
2024-08-27 17:06:48 -0300

    ¯\_(ツ)_/¯

--- app.js
+++ app.js
@@ -12,5 +12,9 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
+app.use((req, res, next) => {
+    res.status(404).render("404");
+});
+
 const PORT = process.env.APP_PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

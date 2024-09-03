3414dde3bc612336836d64e0d2057314229002d1
Guilherme Eduardo Bublitz <gui.e.bublitz@gmail.com>
2024-09-03 07:48:18 -0300

    Block while development

--- app.js
+++ app.js
@@ -8,17 +8,19 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-    res.render("home");
-});
+// app.get("/", (req, res) => {
+//     res.render("home");
+// });
 
-app.get("/about", (req, res) => {
-    res.render("about");
-});
+// app.get("/about", (req, res) => {
+//     res.render("about");
+// });
 
 app.use((req, res, next) => {
     res.status(404).render("404");
+    res.status(403).send("Access Forbidden: This site is currently under development.");
 });
+
 
 const PORT = process.env.APP_PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
